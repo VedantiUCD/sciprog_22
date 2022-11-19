81a060b918fe864388915fd3bd1e9de9650e713e
Vedanti Borate <vedanti.borate@ucdconnect.ie>
2022-11-19 15:53:18 +0000

    commiting all modified files for Prac-2

---
+++ Pratical 2/Conversion.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+/* Declare variables */
+   int i,num,temp,numdigits;
+   float fnum;
+   char binnum[60];
+
+/* Intialise 4-byte integer */
+   num = 33554431;
+/* Convert to 4-byte float */
+   fnum = (float) num;
+
+
+/* Convert to binary number (string)*/
+   i = 0; 
+   temp = num;
+   //while loop to process all elements and convert to binary
+   while (temp > 0) 
+   {
+     sprintf(&binnum[i],"%1d",temp%2);
+     temp = temp/2;
+     i++;
+   }
+
+/* Terminate the string */
+   binnum[i] = '\0';
+       
+
+/* Completing the code for calculations */
+      numdigits = ceil(logf(fnum)/logf(2));
+      //printf("Enter a numeric digit: \n ");
+      //scanf("%d",&num);
+      printf("The number of digits is %d\n",numdigits);
+      printf("Number is: num=%d,  Floating number is: fnum=%f, num in binary=%s\n",
+      num,fnum,binnum);
+
+}
---
+++ Pratical 2/Sum.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+int main(void) {
+/* Declare variables */
+   int i;
+   float sum1, sum2, diff;
+   
+
+/* First sum */
+   sum1 = 0.0;
+   for (i=1; i<=1000; i++) {
+      //calculating the sum using the equation provided when loop variable is in ascending order
+      sum1=sum1+(1/(float)i);
+   }
+
+
+/* Second sum */
+   sum2 = 0.0;
+   for (i=1000; i>0; i--) {
+       //calculating the sum using the equation provided when loop variable is in descending order
+      sum2=sum2+(1/(float)i);
+   }
+
+   printf(" Sum1=%f\n",sum1);
+   printf(" Sum2=%f\n",sum2);
+
+/* Find the difference */
+   diff = sum1-sum2;
+   printf(" Difference between the two is %f\n",diff);
+   if(sum1!=sum2)
+   {
+      printf(" Results for both summation are not same");
+   }
+   //There is a difference between sum 1 and sum2  = 0.000007
+
+}
